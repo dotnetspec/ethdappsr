af47d3d7eeec560bbfd23a098cbd342b1f2f87ea
dotnetspec <prmockridge@gmail.com>
2018-08-30 18:11:51 +0800

    Switch to smaller xml

--- app/js/xml-doc-ops.js
+++ app/js/xml-doc-ops.js
@@ -1,16 +1,16 @@
 var x,xmlhttp,xmlDoc
 var txt = "0";
 xmlhttp = new XMLHttpRequest();
-//xmlhttp.open("GET", "xml/cd_catalog.xml", false);
-xmlhttp.open("GET", "xml/individual_rankings_long.xml", false);
+//xmlhttp.open("GET", "xml/individual_rankings_long.xml", false);
+xmlhttp.open("GET", "xml/individual_rankings.xml", false);
 xmlhttp.send();
 xmlDoc = xmlhttp.responseXML;
-x = xmlDoc.getElementsByTagName("CD");
+x = xmlDoc.getElementsByTagName("PLAYER");
 table="<tr><th>PLAYER</th><th>Rank</th></tr>";
 for (i = 0; i <x.length; i++) {
 	x[i].getElementsByTagName("RANK")[0].childNodes[0].nodeValue = txt;
   table += "<tr onclick='checkMyChallengeOpponent(" + i + ")'><td>";
-  table += x[i].getElementsByTagName("PLAYER")[0].childNodes[0].nodeValue;
+  table += x[i].getElementsByTagName("NAME")[0].childNodes[0].nodeValue;
   table += "</td><td>";
   table +=  x[i].getElementsByTagName("RANK")[0].childNodes[0].nodeValue;
 	table += "</td><td>" +
