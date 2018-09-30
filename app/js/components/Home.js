42939d5fa5aa78ad51edf0688e77e721bb0d2374
dotnetspec <prmockridge@gmail.com>
2018-09-30 20:43:35 +0800

    Select table integration

--- app/js/components/Home.js
+++ app/js/components/Home.js
@@ -1,8 +1,6 @@
 import React, { Component } from 'react';
 import {Grid, Row, Col, PageHeader} from 'react-bootstrap';
-import ReactTable from 'react-table';
-import "react-table/react-table.css";
-import Rankjsondata from '../../json/Rankings.json'
+import SelectTable from './SelectTable'
 
 /**
  * Class representing the home page rendering
@@ -14,53 +12,12 @@ class Home extends Component{
   //#region Constructor
   constructor(props){
     super(props);
-    //this.state = {title: "the title",};
   }
-
 
   //#endregion
 
-  //const title = "Philip Here";
-
-  // changeTitle(title){
-  //   this.setState({title});
-  // }
-  //
-  // handleChange(e){
-  //   const title = e.target.value;
-  //   this.changeTitle(title);
-  // }
-
   //#region React lifecycle events
   render() {
-
-    const data = Rankjsondata;
-
-    // [{
-    //   name: 'Tanner Linsley',
-    //   age: 26,
-    //   friend: {
-    //     name: 'Jason Maurer',
-    //     age: 23,
-    //   }
-    // }]
-
-    const columns = [{
-      Header: 'Name',
-      accessor: 'NAME' // String-based value accessors!
-    }, {
-      Header: 'Rank',
-      accessor: 'RANK',
-      Cell: props => <span className='number'>{props.value}</span> // Custom cell components!
-    }, {
-      id: 'id', // Required because our accessor is not a string
-      Header: 'Id',
-      accessor: d => d.id // Custom value accessors!
-    }, {
-      Header: props => <span>Rank</span>, // Custom header components!
-      accessor: 'RANK'
-    }]
-
 
     return (
       <div>
@@ -74,11 +31,8 @@ class Home extends Component{
         </Row>
         <Row>
           <Col xs={12}>
-          <ReactTable
-                    data={data}
-                    columns={columns}
+          <SelectTable
         />
-
         </Col>
       </Row>
       </Grid>
