e2ab91a5e0b0b5d4efeb66e40772a1cb9389ea5f
dotnetspec <prmockridge@gmail.com>
2018-10-15 17:57:53 +0800

    Embark 3.2.3 working, remembering usernames - modal show glitch

--- app/js/dapp.js
+++ app/js/dapp.js
@@ -3,12 +3,12 @@ import { BrowserRouter } from 'react-router-dom'
 import App from './components/App';
 import React from 'react';
 import EmbarkJS from 'Embark/EmbarkJS';
-import DTwitter from 'Embark/contracts/DTwitter';
+//import DTwitter from 'Embark/contracts/DTwitter';
 import DSportRank from 'Embark/contracts/DSportRank';
 
 
 window.EmbarkJS = EmbarkJS;
-window.DTwitter = DTwitter;
+//window.DTwitter = DTwitter;
 window.DSportRank = DSportRank;
 
 render((
