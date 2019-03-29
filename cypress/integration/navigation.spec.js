279807f2cf2942d048ff7bb1b0c91764bdd648f9
dotnetspec <prmockridge@gmail.com>
2019-03-29 12:23:54 +0800

    nav update and add network requests

--- cypress/integration/navigation.spec.js
+++ cypress/integration/navigation.spec.js
@@ -5,13 +5,13 @@ context('Navigation', () => {
     //cy.visit('https://example.cypress.io')
     // cy.get('.navbar-nav').contains('Commands').click()
     // cy.get('.dropdown-menu').contains('Navigation').click()
-    //cy.RankingSeedViaGlobalViewBtn()
+    cy.RankingSeedViaGlobalViewBtn()
   })
 
-  it.only('cy.go() - go back or forward in the browser\'s history', () => {
+  it('cy.go() - go back or forward in the browser\'s history', () => {
     // https://on.cypress.io/go
 
-    cy.RankingSeedViaGlobalViewBtn()
+    //cy.RankingSeedViaGlobalViewBtn()
 
     cy.location('pathname').should('include', 'player1')
 
@@ -38,22 +38,22 @@ context('Navigation', () => {
     cy.reload(true)
   })
 
-  it('cy.visit() - visit a remote url', () => {
-    // https://on.cypress.io/visit
-
-    // Visit any sub-domain of your current domain
-
-    // Pass options to the visit
-    cy.visit('https://example.cypress.io/commands/navigation', {
-      timeout: 50000, // increase total time for the visit to resolve
-      onBeforeLoad (contentWindow) {
-        // contentWindow is the remote page's window object
-        expect(typeof contentWindow === 'object').to.be.true
-      },
-      onLoad (contentWindow) {
-        // contentWindow is the remote page's window object
-        expect(typeof contentWindow === 'object').to.be.true
-      },
-    })
-    })
+  // it.only('cy.visit() - visit a remote url', () => {
+  //   // https://on.cypress.io/visit
+  //
+  //   // Visit any sub-domain of your current domain
+  //
+  //   // Pass options to the visit
+  //   cy.visit('https://example.cypress.io/commands/navigation', {
+  //     timeout: 50000, // increase total time for the visit to resolve
+  //     onBeforeLoad (contentWindow) {
+  //       // contentWindow is the remote page's window object
+  //       expect(typeof contentWindow === 'object').to.be.true
+  //     },
+  //     onLoad (contentWindow) {
+  //       // contentWindow is the remote page's window object
+  //       expect(typeof contentWindow === 'object').to.be.true
+  //     },
+  //   })
+  //   })
 })
