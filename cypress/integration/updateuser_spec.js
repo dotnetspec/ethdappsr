065cf022b70654e263eacb10e8cda631b5ef85e2
dotnetspec <prmockridge@gmail.com>
2019-04-03 17:31:43 +0800

    Updateuser route fixed

--- cypress/integration/updateuser_spec.js
+++ cypress/integration/updateuser_spec.js
@@ -44,7 +44,12 @@ describe("User Profile Test", function(){
       // cy.contains('Update Profile').click()
     })
     it('Returns to home on submit', () => {
-      cy.route('POST','https://localhost:8000/json', [{ "RANKINGNAME":"testRank1","RANKINGDESC":"testRank","ACTIVE":true,"RANKINGID":"5c6a7cf5a83a2931773847b8"},{"RANKINGID":"5c6a81756874aa33ba152e56","ACTIVE":true,"RANKINGDESC":"test","RANKINGNAME":"test11"},{"RANKINGNAME":"testranking2","RANKINGDESC":"test2","ACTIVE":true,"RANKINGID":"5c81c1e944e81057efe3e2c8"},{"RANKINGID":"5c87394bbb08b22a75685941","ACTIVE":true,"RANKINGDESC":"tl3","RANKINGNAME":"testLadder3"},{"RANKINGNAME":"mplayer1rank","RANKINGDESC":"mp1r","ACTIVE":true,"RANKINGID":"5c875c79adeb832d3ec6732d" }])// cy.get('.description').clear()
+      cy.route('POST','https://localhost:8000/json',
+      [{ "RANKINGNAME":"testRank1","RANKINGDESC":"testRank","ACTIVE":true,"RANKINGID":"5c6a7cf5a83a2931773847b8"},
+      {"RANKINGID":"5c6a81756874aa33ba152e56","ACTIVE":true,"RANKINGDESC":"test","RANKINGNAME":"test11"},
+      {"RANKINGNAME":"testranking2","RANKINGDESC":"test2","ACTIVE":true,"RANKINGID":"5c81c1e944e81057efe3e2c8"},
+      {"RANKINGID":"5c87394bbb08b22a75685941","ACTIVE":true,"RANKINGDESC":"tl3","RANKINGNAME":"testLadder3"},
+      {"RANKINGNAME":"mplayer1rank","RANKINGDESC":"mp1r","ACTIVE":true,"RANKINGID":"5c875c79adeb832d3ec6732d" }])// cy.get('.description').clear()
       // .type('test description')
       // .should('have.value', 'test description')
       //do something as otherwise it's supposed to do nothing currently
